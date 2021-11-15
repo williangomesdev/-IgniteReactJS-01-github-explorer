f3e7ad6577056aa9698be426351e494eb181d4b4
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-11-15 14:57:12 -0300

    Projeto finalizado

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV != "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ].filter(Boolean),
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
